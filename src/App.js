e7fe8e76265fa21a7e1474e59ce4724466fd9cc7
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-10-15 07:38:55 -0300

    - aula 33

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 import React from 'react';
 
-
-const Bemvindo = (props) => {
+const Equipe = (props) => {
     return(
         <div>
-            <h2>Bem vindo {props.nome}</h2>
-            <h3>Tenho {props.idade} anos</h3>
+            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
+            <Midias fb={props.facebook} />
+        </div>
+    );
+}
+
+const Sobre = (props) =>{
+    return(
+        <div>
+            <h2>Ola sou a(o) {props.nome}</h2>
+            <h3>Cargo: {props.cargo}</h3>
+            <h3>Idade: {props.idade}</h3>
+        </div>
+    );
+}
+
+const Midias = (props) =>{
+    return(
+        <div>
+            <a href={props.fb}>Facebook</a>
+            <a>Youtube</a>
+            <a>Instagran</a>
         </div>
     );
 }
 function App(){
     return (
         <div>
-            <h1>Ola Mundo</h1>
-            <Bemvindo nome="Naira" idade="17" />
-            <Bemvindo nome="Sergio" idade="40"/>
+            <h1>Conheça nossa equipe</h1>
+            <Equipe nome="Naira" cargo="Progamadora" idade="16" facebook="http://www.google.com.br"/>
+            <Equipe nome="Sergio" cargo="Progamador" idade="40"/>
         </div>
     );
 }
