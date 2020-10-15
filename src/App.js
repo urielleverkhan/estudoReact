20b06052d684467f836c3b157e966b7d58eb57f8
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-10-15 07:48:31 -0300

    -aula 34 class component

--- src/App.js
+++ src/App.js
@@ -1,39 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const Equipe = (props) => {
-    return(
-        <div>
-            <Sobre nome={props.nome} cargo={props.cargo} idade={props.idade} />
-            <Midias fb={props.facebook} />
-        </div>
-    );
-}
-
-const Sobre = (props) =>{
-    return(
-        <div>
-            <h2>Ola sou a(o) {props.nome}</h2>
-            <h3>Cargo: {props.cargo}</h3>
-            <h3>Idade: {props.idade}</h3>
-        </div>
-    );
-}
-
-const Midias = (props) =>{
-    return(
-        <div>
-            <a href={props.fb}>Facebook</a>
-            <a>Youtube</a>
-            <a>Instagran</a>
-        </div>
-    );
+class Equipe extends Component{
+    render(){
+        return(
+            <div>
+                <h2>Ola sou a {this.props.nome}</h2>
+                <h3>Cargo: {this.props.cargo}</h3>
+            </div>
+        );
+    }
 }
 function App(){
     return (
         <div>
             <h1>Conheça nossa equipe</h1>
-            <Equipe nome="Naira" cargo="Progamadora" idade="16" facebook="http://www.google.com.br"/>
-            <Equipe nome="Sergio" cargo="Progamador" idade="40"/>
+            <Equipe nome="Naira" cargo="programadora"/>
+            
         </div>
     );
 }
