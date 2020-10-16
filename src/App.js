6900d63f9010ba7bb8a073c3a6999f29bdba2608
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-10-16 11:22:42 -0300

    -aula 40, renderezação condicional

--- src/App.js
+++ src/App.js
@@ -3,12 +3,35 @@ import React, {Component} from 'react';
 import Membro from './componentes/membro/Membro.js'
 
 class App extends Component{
- 
- 
+    constructor(props){
+        super(props);
+        this.state = {
+            status: false
+        };
+        this.entrar = this.entrar.bind(this);
+        this.sair = this.sair.bind(this);
+    }
+    entrar(){
+        this.setState({status: true});
+    }
+    sair(){
+        this.setState({status: false});
+    }
     render(){
         return(
             <div>
-                <Membro nome="Visitante"/>
+               {this.state.status ?
+                <div><h1> Bem vindo ao sistema</h1>
+                <button onClick={this.sair}>Sair</button>
+                </div> :
+                <div><h1>Não logado</h1>
+                <button onClick={this.entrar}>Entrar</button>
+                </div>
+
+               }
+               <div>
+                   <h2>Curso React Js</h2>
+               </div>
                 
             </div>
         );
