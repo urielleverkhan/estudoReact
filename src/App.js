b684bd19781cda60d76614f520f8f7d2bf382c0b
Naira Magalhães <52166063+urielleverkhan@users.noreply.github.com>
2020-10-17 15:43:27 -0300

    - aula 46 cronometro

--- src/App.js
+++ src/App.js
@@ -7,47 +7,56 @@ class App extends Component{
     constructor (props){
         super(props);
         this.state = {
-            textoFrase:''
+           numero: 0,
+           txBotao: "Começar"
         };
-        this.frases = ['Siga os bons e aprenda com eles.', 'O bom-senso vale mais do que muito conhecimento.', 
-        'O riso é a menor distância entre duas pessoas.', 
-        'Deixe de lado as preocupações e seja feliz.',
-        'Realize o óbvio, pense no improvável e conquiste o impossível.',
-        'Acredite em milagres, mas não dependa deles.',
-        'A maior barreira para o sucesso é o medo do fracasso.'];
-
-        this.quebraBiscoito = this.quebraBiscoito.bind(this);
+        this.timer = null;
+        this.comecar = this.comecar.bind(this);
+        this.limpar = this.limpar.bind(this);
     }
 
-    quebraBiscoito(){
+  comecar(){
+      let state = this.state;
+
+      if(this.timer !== null){
+          clearInterval(this.timer);
+          this.timer = null;
+          state.txBotao = 'Começar';
+      }else{
+        this.timer = setInterval(() => {
+            let state = this.state;
+            state.numero += 0.1;
+            this.setState(state);
+        }, 100);
+        state.txBotao = 'Parar';
+    }
+    this.setState(state);
+  }  
+  limpar(){
+        if(this.timer !== null){
+            clearInterval(this.timer);
+            this.timer =null;
+        }
         let state = this.state;
-        let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
-        
-        console.log(this.frases.length);
-        state.textoFrase = '"' + this.frases[numeroAleatorio] +'"';
+        state.numero = 0;
+        state.botao = 'Começar'
         this.setState(state);
-    }
+  }
    
     
     render(){
         return(
             <div className="container">
-                <img src={require('./assets/biscoito.png')} className="img"/>
-                <Botao nome="Abrir biscoito" acaoBtn={this.quebraBiscoito}/>
-                <h3 className="textoFrase">{this.state.textoFrase}</h3>
+               <img src={require('./assets/cronometro.png')} className="img"/>
+               <a className="timer">{this.state.numero.toFixed(1)}</a>
+               <div className="areaBtn">
+                   <a className="botao" onClick={this.comecar}>{this.state.txBotao}</a>
+                   <a className="botao" onClick={this.limpar}>Limpar</a>
+               </div>
             </div>
         );
     }
 
 }
 
-class Botao extends Component{
-    render(){
-        return(
-            <div>
-                <button onClick={this.props.acaoBtn}>{this.props.nome}</button>
-            </div>
-        );
-    }
-}
 export default App;
